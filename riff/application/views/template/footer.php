34f620c44cab14d46c4d4edfce485bf64cbb7397
rifkimuhammad <rifkimhmd44@gmail.com>
2022-04-15 14:32:49 +0700

    riffhotel

---
+++ riff/application/views/template/footer.php
@@ -0,0 +1,9 @@
+<footer class="tm-black-bg">
+		<div class="container">
+			<div class="row">
+				<p class="tm-copyright-text">HOTEL &copy; RIFFHOTEL 
+                
+                | Desain By rifki</p>
+			</div>
+		</div>		
+	</footer>
